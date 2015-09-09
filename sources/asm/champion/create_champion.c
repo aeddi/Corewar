878ca535c2c089de6ba11fe3cf970c190de552a1
Godefroy PONSINET <gponsine@e3r5p13.42.fr>
2015-09-09 14:39:52 +0200

    debug

--- sources/asm/champion/create_champion.c
+++ sources/asm/champion/create_champion.c
@@ -13,63 +13,12 @@
 #include "champion.h"
 #include "op.h"
 
-static int	put_magic_number(int fd)
-{
-	int		magic_nb;
-	char	magic[4];
-
-	ft_putstr("Magic : \t");
-	ft_putnbr(COREWAR_EXEC_MAGIC);
-	ft_putchar('\n');
-	magic_nb = COREWAR_EXEC_MAGIC;
-	magic[0] = magic_nb >> 24 & 0xff;
-	magic[1] = magic_nb >> 16 & 0xff;
-	magic[2] = magic_nb >> 8 & 0xff;
-	magic[3] = magic_nb >> 0 & 0xff;
-	write(fd, magic, 4);
-	return (1);
-}
-
-static int	put_champion_name(t_champ *champion, int fd)
-{
-	char	*name;
-
-	name = ft_strnew(PROG_NAME_LENGTH);
-	ft_putstr("Name : \t\t");
-	if (champion->name)
-	{
-		name = ft_strcpy(name, champion->name);
-		ft_putstr(champion->name);
-	}
-	ft_putchar('\n');
-	write(fd, name, PROG_NAME_LENGTH);
-	free(name);
-	return (1);
-}
-
-static int	put_champion_comment(t_champ *champion, int fd)
-{
-	char	*comment;
-
-	comment = ft_strnew(COMMENT_LENGTH);
-	ft_putstr("Comment : \t");
-	if (champion->comment)
-	{
-		ft_putstr(champion->comment);
-		comment = ft_strcpy(comment, champion->comment);
-	}
-	ft_putchar('\n');
-	write(fd, comment, COMMENT_LENGTH);
-	free(comment);
-	return (1);
-}
-
 int			create_champion(t_champ *champion, char *name)
 {
 	int		fd;
 	char	*filename;
+	t_header	header;
 
-	(void)champion;
 	filename = ft_strrchr(name, 's');
 	filename[0] = '\0';
 	filename = ft_strjoin(name, "cor");
@@ -77,9 +26,12 @@ int			create_champion(t_champ *champion, char *name)
 			S_IRWXU | S_IRGRP | S_IROTH);
 	ft_putstr("\nAssembling : \t");
 	ft_putendl(filename);
-	put_magic_number(fd);
-	put_champion_name(champion, fd);
-	put_champion_comment(champion, fd);
+	ft_bzero(&header, sizeof(t_header));
+	header.magic = ft_revint32(COREWAR_EXEC_MAGIC);
+	ft_strcpy(header.prog_name, champion->name);
+	header.prog_size = ft_revint32(champion->len);
+	ft_strcpy(header.comment, champion->comment);
+	write(fd, &header, sizeof(t_header));
 	ft_putchar('\n');
 	write(fd, champion->bytecode, champion->len);
 	return (1);
